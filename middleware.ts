6fac2454287a9409eeb4b3415b470e3f1dca9716
Oswald-Faust <faust.troukou@epitech.eu>
2024-02-16 15:42:57 +0100

    Chapter & Video is done

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/search"]
+  publicRoutes: ["/", "/search", "/api/webhook"]
 });
 
 export const config = {
